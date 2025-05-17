27753029fdaa562e304496d20726073d407b3f50
Marmaduke23 <javier.lobos@ug.uchile.cl>
2025-05-16 21:51:55 -0400

    Eliminar archivos innecesarios

--- lista_actividades.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll("tr[data-url]").forEach(row => {
-        row.addEventListener("click", function() {
-            window.location.href = this.dataset.url;
-        });
-    });
-});
